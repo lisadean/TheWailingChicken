191299acf983ab7c633e0dfc0a95052912064e33
Lisa Dean <lisa@lisadean.net>
2018-10-17 15:42:24 -0400

    made chicken shake

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 function playSound() {
+  let chicken = document.querySelector('[data-chicken]');
   const audio = document.querySelector('[data-audio]');
   if (audio) {
     audio.currentTime = 0;
     audio.play();
+    chicken.classList.add('playing');
+  }
+}
+
+function removeTransition(e) {
+  if(e.propertyName == 'animation') {
+    this.classList.remove('playing');
   }
 }
 
@@ -10,6 +18,7 @@ function addChickenListener() {
   let chicken = document.querySelector('[data-chicken]');
   if(chicken) {
     chicken.addEventListener('click', playSound);
+    chicken.addEventListener('animationend', removeTransition)
   }
 }
 
